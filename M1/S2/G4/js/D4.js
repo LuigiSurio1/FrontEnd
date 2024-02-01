bcd2200d138ee6e0509fafb587160caebb6e4cc5
LuigiSurio1 <luigisurio1@gmail.com>
2024-02-01 16:54:48 +0100

    js g4

--- M1/S2/G4/js/D4.js
+++ M1/S2/G4/js/D4.js
@@ -2,7 +2,11 @@
  Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function area(l1, l2) {
+    return l1 * l2;
+}
+
+console.log(area(4, 8));
 
 /* ESERCIZIO 2
  Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
@@ -10,21 +14,45 @@
  la loro somma moltiplicata per tre.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function crazySum(a1, a2) {
+    if (a1 === a2) {
+        return (a1 + a2) * 3;
+    } else {
+        return a1 + a2;
+    }
+}
+console.log(crazySum(2, 4));
 
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function crazyDiff(a) {
+    if (a > 19) {
+        return (a / 19) * 3;
+    } else {
+        return a / 19;
+    }
+}
+
+console.log(crazyDiff(30));
 
 /* ESERCIZIO 4
  Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
  se n è uguale a 400.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function boundary(n) {
+    if (n >= 20 && n <= 100) {
+        return 'true';
+    } else if (n = 400) {
+        return 'true';
+    } else {
+        return 'false';
+    }
+}
+console.log(boundary(50))
 
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
@@ -32,37 +60,72 @@
  ritornare la stringa originale senza alterarla.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function epify(p) {
+    if (p.slice(0, 5) === 'EPICODE') {
+        return null;
+    } else {
+        return p.replace('Scuola', 'EPICODE');
+    }
+}
+console.log(epify('Scuola, è molto intensa'));
 
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function check3and7(h) {
+    if (h % 3 === 0 || n % 7 === 0) {
+        return 'true';
+    } else {
+        return 'false'
+    }
+}
+console.log(check3and7(12));
 
 /* ESERCIZIO 7
  Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function reverseString(e) {
+    let splitLetter = e.split("");
+    let reverseLetter = splitLetter.reverse();
+    let joinLetter = reverseLetter.join("");
+    return joinLetter;
+}
+console.log(reverseString('Epicode'));
 
 /* ESERCIZIO 8
  Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
  La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function upperFirst(o) {
+   let newO = o.split(' ');
+   for(i = 0; i < newO.lenght; i++){
+    newO[i] = newO[i][0].toUpperCase() + newO[i].substring(1);
+   }
+   return newO.join(' ');
+}
+console.log(upperFirst('la bella vita'))
 
 /* ESERCIZIO 9
  Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
  della stringa originale.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function cutString(k) {
+    return k.slice(0, k -1);
+   
+}
+console.log(cutString('maiale'));
 
 /* ESERCIZIO 10
  Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function giveMeRandom(j) {
+    let jrandom = (Math.floor(Math.random() * 11));
+    return jrandom;
+}
+console.log(giveMeRandom());
