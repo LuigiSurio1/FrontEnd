4ed0bf99610351aca03f9c9c67dbe92312fbba9b
LuigiSurio1 <luigisurio1@gmail.com>
2024-03-20 16:57:26 +0100

    committino

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { iPosts } from './Models/post';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'G3';
+
+  postsArr:iPosts[] = [];
+
+  ngOnInit(){
+
+    fetch("../assets/db.json")
+    .then(post => post.json())
+    .then(post => {this.postsArr = post.posts
+    this.randomArr()})
+  }
+
+  randomNumber(){
+    let i:number = Math.floor(Math.random() * this.postsArr.length)
+    return i
+  }
+
+  randomArray: iPosts[] = [];
+
+  randomArr(){
+    for (let i = 0; i < 4; i++) {
+      this.randomArray.push(this.postsArr[this.randomNumber()])
+    }
+  }
+
 }
